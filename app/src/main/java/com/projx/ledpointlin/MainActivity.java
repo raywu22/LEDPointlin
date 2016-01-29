ed8cd67082dd3be86d1b8b22e089237c6a16048b
cwillaim <cwwomack@mit.edu>
2016-01-29 01:04:25 -0500

    I changed a lot of stuff but basically we can send post requests

--- app/src/main/java/com/projx/ledpointlin/MainActivity.java
+++ app/src/main/java/com/projx/ledpointlin/MainActivity.java
@@ -1,15 +1,43 @@
 package com.projx.ledpointlin;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.json.*;
+import com.loopj.android.http.*;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.ByteArrayEntity;
+import cz.msebera.android.httpclient.message.BasicHeader;
+import cz.msebera.android.httpclient.protocol.HTTP;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static String IP_ADDRESS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +47,12 @@ public class MainActivity extends AppCompatActivity {
 //        setSupportActionBar(toolbar);
         Intent intent = getIntent();
 
-        String ipAddress = intent.getStringExtra(LandingActivity.EXTRA_RASPBERRYIP);
+        IP_ADDRESS = intent.getStringExtra(LandingActivity.EXTRA_RASPBERRYIP);
         TextView textView = new TextView(this);
         textView.setTextSize(40);
-        textView.setText(ipAddress);
+        textView.setText(IP_ADDRESS);
 
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
-        layout.addView(textView);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     }
 
     @Override
@@ -48,5 +75,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendRequest(View view) throws JSONException, UnsupportedEncodingException {
+        String urlPrefix = "http://";
+        String urlSuffix = ":3000/boardData";
+        String defaultUrl = "10.0.2.2";
+        String baseUrl = (IP_ADDRESS == "") ? IP_ADDRESS : defaultUrl;
+        String url = urlPrefix + baseUrl + urlSuffix;
+        JSONObject params = new JSONObject();
+        params.put("option", "setStartColor");
+        params.put("color", "grey");
+        ByteArrayEntity entity = new ByteArrayEntity(params.toString().getBytes("UTF-8"));
+        entity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post(getApplicationContext(), url, entity, "application/json", new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+
     }
 }
